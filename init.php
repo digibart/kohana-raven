efb9c888a6737676a52c28fa5d3e765cda574373
digibart <pixel@digibart.nl>
2013-08-08 21:08:35 +0200

    fix typo in path to raven-php lib

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ if (Kohana::$errors)
 	set_exception_handler(array('Kohana_Raven_Exception_Handler', 'handler'));
 }
 
-if ($path = Kohana::find_file('vendor', 'raven-php.git/lib/Raven/Client')) {
+if ($path = Kohana::find_file('vendor', 'raven-php/lib/Raven/Client')) {
 	ini_set('include_path',
 	ini_get('include_path').PATH_SEPARATOR.dirname(dirname($path)));
 	
